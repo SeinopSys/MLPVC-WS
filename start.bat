a4742b791de3a2003a4532dad2656669a3ba1d3e
SeinopSys <seinopsys@gmail.com>
2018-03-28 08:28:01 +0200

    Add a post-receive hook

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call forever stop server.js
-forever start server.js
-forever list
--- start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-git reset HEAD --hard
-git pull
-yarn install
-forever list | grep server.js && forever stop server.js
-forever start server.js
-forever list
