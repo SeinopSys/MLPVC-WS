736f4e45017801b9b8b5ec2eb4be0cf02f8c2558
SeinopSys <seinopsys@gmail.com>
2018-04-19 23:29:21 +0200

    Switch to PM2

--- post-receive.sh
+++ post-receive.sh
@@ -3,12 +3,11 @@ echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
-CMD_CD="cd $(readlink -nf "$PWD/..")"
+CMD_CD="cd .. && echo 'Changed directory to $PWD'"
 CMD_FETCH="env -i git fetch"
 CMD_YARN="sudo -u www-data yarn install --production"
-CMD_FOREVER_STOP="forever list | grep server.js && forever stop server.js"
-CMD_FOREVER_START="forever start server.js"
-CMD_FOREVER_LIST="forever list"
+CMD_STOP="sudo -u www-data pm2 stop pm2.json"
+CMD_START="sudo -u www-data pm2 start pm2.json"
 
 echo "$ $CMD_CD"
 eval ${CMD_CD}
@@ -16,11 +15,9 @@ echo "$ $CMD_FETCH"
 eval ${CMD_FETCH}
 echo "$ $CMD_YARN"
 eval ${CMD_YARN}
-echo "$ $CMD_FOREVER_STOP"
-eval ${CMD_FOREVER_STOP}
-echo "$ $CMD_FOREVER_START"
-eval ${CMD_FOREVER_START}
-echo "$ $CMD_FOREVER_LIST"
-eval ${CMD_FOREVER_LIST}
+echo "$ $CMD_STOP"
+eval ${CMD_STOP}
+echo "$ $CMD_START"
+eval ${CMD_START}
 
 echo "##### end post-receive hook #####"
