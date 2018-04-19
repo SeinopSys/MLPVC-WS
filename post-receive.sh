d5065e17c85a8dcfc5160a6fe031d2644edff0d2
SeinopSys <seinopsys@gmail.com>
2018-04-19 23:46:59 +0200

    Fix cd command in post-receive hook

--- post-receive.sh
+++ post-receive.sh
@@ -3,21 +3,21 @@ echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
-CMD_CD="cd $(readlink -nf \"$GTI_DIR/..\")"
+CMD_PWD="pwd"
 CMD_FETCH="env -i git fetch"
 CMD_YARN="sudo -u www-data yarn install --production"
 CMD_STOP="sudo -u www-data pm2 stop pm2.json"
 CMD_START="sudo -u www-data pm2 start pm2.json"
 
-echo "$ $CMD_CD"
-eval ${CMD_CD}
+echo "$ $CMD_PWD"
+eval $CMD_PWD
 echo "$ $CMD_FETCH"
-eval ${CMD_FETCH}
+eval $CMD_FETCH
 echo "$ $CMD_YARN"
-eval ${CMD_YARN}
+eval $CMD_YARN
 echo "$ $CMD_STOP"
-eval ${CMD_STOP}
+eval $CMD_STOP
 echo "$ $CMD_START"
-eval ${CMD_START}
+eval $CMD_START
 
 echo "##### end post-receive hook #####"
