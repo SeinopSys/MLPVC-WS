a3e232095ca9d5d478b321d72b6015fc991650ca
SeinopSys <seinopsys@gmail.com>
2018-04-20 03:00:47 +0200

    Improve post-receive hook

--- post-receive.sh
+++ post-receive.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "##### post-receive hook #####"
 read oldrev newrev refname
 echo "Push triggered update to revision $newrev ($refname)"
 
@@ -27,5 +26,3 @@ if $GIT diff --name-only $oldrev $newrev | grep "^server.js"; then
 else
 	echo "# Skipping server restart, serve file not modified"
 fi
-
-echo "##### end post-receive hook #####"
